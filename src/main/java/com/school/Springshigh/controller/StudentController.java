215ca9cd2134ea3d45569ab2255b4c0717f6190c
mtakadev <mtakadev010@gmail.com>
2025-01-25 19:28:02 +0200

    Login functionality

--- src/main/java/com/school/Springshigh/controller/StudentController.java
+++ src/main/java/com/school/Springshigh/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.school.Springshigh.controller;
 import com.school.Springshigh.entity.Student;
 import com.school.Springshigh.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/students")
@@ -23,4 +26,15 @@ public class StudentController {
         Student savedStudent = studentRepository.save(student);
         return ResponseEntity.ok(savedStudent);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
+        Optional<Student> student = studentRepository.findByEmail(credentials.get("email"));
+        if (student.isPresent() && student.get().getPassword().equals(credentials.get("password"))) {
+            return ResponseEntity.ok("Login successful");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
+// TODO: 2FA Implementation
 }
--- src/main/java/com/school/Springshigh/entity/Student.java
+++ src/main/java/com/school/Springshigh/entity/Student.java
@@ -16,4 +16,44 @@ public class Student {
     private String email;
     private String password;
     private LocalDate enrollmentDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
 }
